6ff86c88c76c0fa2b8e963a6f30fe1d11a6ae446
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-07-23 12:30:59 +0600

    footer

--- src/Component/Layout/Main.jsx
+++ src/Component/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar";
+import Footar from "../Shared/Footar";
 
 const Main = () => {
     return (
         <div>
           <Navbar></Navbar>
-          <Outlet></Outlet>
+          <Outlet></Outlet> 
+          <Footar></Footar>
         </div>
     );
 };
